886d1d32e76da499398b408b0a29ecde9bf044eb
Pedro de Almeida <sr.pedrodealmeida@gmail.com>
2023-08-01 20:46:05 -0300

    app running 00

--- three-js/js/main.js
+++ three-js/js/main.js
@@ -6,7 +6,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+//document.body.appendChild( renderer.domElement );
 
 //criar o objeto
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
